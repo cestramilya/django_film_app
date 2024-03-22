4e18f0996c526299b181d9191a1fae2396d7dfbe
cestramilya <aubakirova.ramilya@gmail.com>
2024-03-22 12:05:53 +0500

    Update settings.py

--- cine_navigate/settings.py
+++ cine_navigate/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cr8b3ah-i8@5use6*@gvqdq_ni+cw^=c@tkya$zukr0v%4_w*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
